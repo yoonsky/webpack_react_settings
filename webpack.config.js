b3b73235ca0e66555c544b34879ba7c193c71ea0
shimyuseob <shimyuseob@gmail.com>
2021-01-10 02:08:18 +0900

    ts-checking-plugin error

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,35 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = {
-  entry: {
-    'js/app': ['./src/App.tsx'],
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist/'),
-    publicPath: '/',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        use: [
-          'babel-loader',
-          {
-            loader: 'ts-loader',
-            options: {
-              transpileOnly: true,
-            },
-          },
-        ],
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html',
-    }),
-    new ForkTsCheckerWebpackPlugin({ silent: true }),
-  ],
+	entry: {
+		'js/app': ['./src/App.tsx'],
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist/'),
+		publicPath: '/',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(ts|tsx)$/,
+				use: [
+					'babel-loader',
+					{
+						loader: 'ts-loader',
+						options: {
+							transpileOnly: true,
+						},
+					},
+				],
+				exclude: /node_modules/,
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+			filename: 'index.html',
+		}),
+	],
 };
